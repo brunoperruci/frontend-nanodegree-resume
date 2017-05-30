c2ffff33bace63d3f3fde140e39ce46d427b424b
Bruno Perruci <Bruno Perruci>
2017-05-29 22:06:51 -0300

    Add Google Maps and Fixed the Image Size

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
  var bio = {
    "name": "Bruno Perruci",
    "role": "UI Designer",
-   "contact": {
+   "contacts": {
      "mobile": "+55 98276-6799",
      "email": "brunoperruci@gmail.com",
      "github": "brunoperruci",
@@ -19,6 +19,7 @@
        "city": "Olinda - PE",
        "degree": "Bachelors",
        "majors": "Graphic Design",
+       "location": "AESO - Barros Melo",
        "dates": 2012
      },
      {
@@ -26,6 +27,7 @@
        "city": "Recife - PE",
        "degree": "Specialization",
        "majors": "Start",
+       "location": "SAGA School of Art Gaming and Animation, Recife",
        "dates": 2011
      }
    ],
@@ -45,18 +47,21 @@
        "employer": "Upwork",
        "title": "Graphic Designer",
        "dates": "January, 2017 - Actual",
+       "location": "Bairro Novo, Olinda",
        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in luctus mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum leo eu rutrum faucibus. Sed rhoncus ipsum varius, efficitur tortor et, tincidunt enim. Maecenas mollis et turpis vel vulputate. Duis porttitor pellentesque felis non malesuada. Mauris at purus nec nisl malesuada placerat. Sed sagittis pharetra est, eu tristique ex interdum non. Interdum et malesuada fames ac ante ipsum primis in faucibus."
      },
      {
        "employer": "Th9",
        "title": "Creative Art Director",
        "dates": "October, 2013 - January, 2015",
+       "location": "Universidade Federal Rural de Pernambuco",
        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in luctus mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum leo eu rutrum faucibus. Sed rhoncus ipsum varius, efficitur tortor et, tincidunt enim. Maecenas mollis et turpis vel vulputate. Duis porttitor pellentesque felis non malesuada. Mauris at purus nec nisl malesuada placerat. Sed sagittis pharetra est, eu tristique ex interdum non. Interdum et malesuada fames ac ante ipsum primis in faucibus."
      },
      {
        "employer": "3Pontos Comunicação",
        "title": "Creative Assistant",
        "dates": "January, 2012 - August, 2013",
+       "location": "Praça de Casa Forte",
        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in luctus mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum leo eu rutrum faucibus. Sed rhoncus ipsum varius, efficitur tortor et, tincidunt enim. Maecenas mollis et turpis vel vulputate. Duis porttitor pellentesque felis non malesuada. Mauris at purus nec nisl malesuada placerat. Sed sagittis pharetra est, eu tristique ex interdum non. Interdum et malesuada fames ac ante ipsum primis in faucibus."
      }
    ]
@@ -80,8 +85,6 @@
        "description": "Diligento is a mobile app which finds remote legal partners.",
        "images": [
          "https://mir-s3-cdn-cf.behance.net/project_modules/max_3840/73de7043973877.5803fc48cbe5d.jpg",
-         "https://mir-s3-cdn-cf.behance.net/project_modules/1400/ccb43d43973877.5803fc4905290.gif",
-         "https://mir-s3-cdn-cf.behance.net/project_modules/max_3840/ab18e243973877.5803fc49058f1.jpg",
          "https://mir-s3-cdn-cf.behance.net/project_modules/max_3840/905b1743973877.5803fc4906010.jpg"
        ]
      }
@@ -90,20 +93,16 @@
 
   var formattedName = HTMLheaderName.replace("%data%", bio.name);
   var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-  // var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
-  // var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
+  var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+  var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
   var formattedPic = HTMLbioPic.replace("%data%", bio.picture);
-  // var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-  // var formattedSkills = HTMLskills.replace("%data%", bio.skills);
-  //
+
+    //
   $("#header").prepend(formattedRole);
   $("#header").prepend(formattedName);
-  // $("#header").append(formattedEmail);
-  // $("#header").append(formattedMobile);
+  $("#header").append(formattedEmail);
+  $("#header").append(formattedMobile);
   $("#header").append(formattedPic);
-  // $("#header").append(formattedWelcome);
-  // $("#header").append(HTMLskillsStart);
-  // $("#header").append(formattedSkills);
 
 
   if(bio.skills.length > 0) { //skills display
@@ -130,35 +129,6 @@ function displayWork() {
 } //work display //work display
 displayWork();
 
-$(document).click(function(loc) {
-   // your code goes here
-   var x = loc.pageX;
-   var y = loc.pageY;
-   logClicks(x,y);
- });
-
-/* InternationalizeButton \*
- $("#main").append(internationalizeButton);
- function inName() {
-   var internationalName = bio.name.split(" ");
-   var firstName = internationalName[0][0].toUpperCase() + internationalName[0].slice(1).toLowerCase();
-   var secondName = internationalName[1].toUpperCase();
-   return firstName + " " + secondName;
-}
-*/
-// teacher code (not working)
-// projects.display = function() {
-//   for (project in projects.projects) {
-//     $("#projects").append(HTMLprojectStart);
-//     var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-//     $(".project-entry:last").append(formattedTitle);
-//     var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
-//     $(".project-entry:last").append(formattedDates);
-//     var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
-//     $(".project-entry:last").append(formattedDescription);
-//   }
-// }
-
 projects.display = function() {
   for (i = 0; i <= projects.projects.length - 1; i++) {
     $("#projects").append(HTMLprojectStart);
@@ -177,3 +147,16 @@ projects.display = function() {
   }
 }
 projects.display();
+
+$("img").css("max-height", "250px");
+
+$("#mapDiv").append(googleMap);
+
+/*Log Clicks button
+$(document).click(function(loc) {
+   // your code goes here
+   var x = loc.pageX;
+   var y = loc.pageY;
+   logClicks(x,y);
+ });
+*/
